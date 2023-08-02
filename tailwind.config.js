967ee2970bf2ceacc5e612f6f7cdebd79734dc67
code-a-man <43219246+code-a-man@users.noreply.github.com>
2023-08-02 17:26:31 +0300

    🎉 Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,22 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        brown: {
+          50: '#fdf8f6',
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+          light: '#f2e8e5',
+          dark: '#43302b',
+        },
+      }
     },
   },
   plugins: [],

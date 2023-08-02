c82fd4498ce764520a9df6901645f9a19788d219
code-a-man <43219246+code-a-man@users.noreply.github.com>
2023-08-02 17:26:31 +0300

    🎉 Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA()

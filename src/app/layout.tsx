60ed79c60ca700920859eeba78e910649212a4c7
Ramazan Sancar <ramazansancar4545@gmail.com>
2025-02-07 00:02:27 +0300

    fix: Fixed regex check for null incoming data in prod mode.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/favicon.ico" />
       <body className={`${inter.className} bg-brown-dark`}>{children}</body>
     </html>
   )
